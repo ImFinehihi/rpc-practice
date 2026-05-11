369dbf4c9f329f7b3490ad7d4cab1c3c9d36fd46
ImFinehihi <23010623@st.phenikaa-uni.edu.vn>
2026-05-11 12:04:58 +0700

    Update client.py

--- xmlrpc/client.py
+++ xmlrpc/client.py
@@ -1,20 +1,6 @@
-with SimpleXMLRPCServer(('localhost', 8000),
-                        requestHandler=RequestHandler) as server:
-    server.register_introspection_functions()
+import xmlrpc.client
 
-    
-    # Register a function under a different name
-    def adder_function(x, y):
-        return x + y
-    server.register_function(adder_function, 'add')
+s = xmlrpc.client.ServerProxy('http://localhost:8000')
 
-    # Register an instance; all the methods of the instance are
-    # published as XML-RPC methods (in this case, just 'mul').
-    class MyFuncs:
-        def mul(self, x, y):
-            return x * y
-
-    server.register_instance(MyFuncs())
-
-    # Run the server's main loop
-    server.serve_forever()
+print(s.add(2,3))  # Returns 5
+print(s.mul(5,2))  # Returns 5*2 = 10
