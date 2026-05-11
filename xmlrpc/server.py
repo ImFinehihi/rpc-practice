1b211f5bf09db0e2b851d9d7d4d1620bc590902d
ImFinehihi <23010623@st.phenikaa-uni.edu.vn>
2026-05-12 06:58:12 +0700

    Update server.py

--- xmlrpc/server.py
+++ xmlrpc/server.py
@@ -1,20 +1,32 @@
-with SimpleXMLRPCServer(('localhost', 8000),
-                        requestHandler=RequestHandler) as server:
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCRequestHandler
+
+# Restrict to a particular path
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+# Create server
+with SimpleXMLRPCServer(
+    ('0.0.0.0', 8000),
+    requestHandler=RequestHandler
+) as server:
+
     server.register_introspection_functions()
 
-    
-    # Register a function under a different name
+    # Hàm cộng
     def adder_function(x, y):
         return x + y
+
     server.register_function(adder_function, 'add')
 
-    # Register an instance; all the methods of the instance are
-    # published as XML-RPC methods (in this case, just 'mul').
+    # Class nhân
     class MyFuncs:
         def mul(self, x, y):
             return x * y
 
     server.register_instance(MyFuncs())
 
-    # Run the server's main loop
+    print("Server đang chạy tại cổng 8000...")
+
+    # Run server
     server.serve_forever()
