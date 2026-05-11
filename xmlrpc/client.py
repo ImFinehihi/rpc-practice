20cdd337940f4c1c55e9123996821180e44f7cd1
ImFinehihi <23010623@st.phenikaa-uni.edu.vn>
2026-05-11 12:03:57 +0700

    Update client.py

--- xmlrpc/client.py
+++ xmlrpc/client.py
@@ -1,7 +1,20 @@
-import xmlrpc.client
+with SimpleXMLRPCServer(('localhost', 8000),
+                        requestHandler=RequestHandler) as server:
+    server.register_introspection_functions()
 
-proxy = xmlrpc.client.ServerProxy("http://localhost:8000/")
+    
+    # Register a function under a different name
+    def adder_function(x, y):
+        return x + y
+    server.register_function(adder_function, 'add')
 
-result = proxy.add(5, 3)
+    # Register an instance; all the methods of the instance are
+    # published as XML-RPC methods (in this case, just 'mul').
+    class MyFuncs:
+        def mul(self, x, y):
+            return x * y
 
-print("Kết quả:", result)
+    server.register_instance(MyFuncs())
+
+    # Run the server's main loop
+    server.serve_forever()
